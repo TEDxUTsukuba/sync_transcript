13c302a269b8fda79ffa844d8feb987c63a6e0ff
makiart <haruki3968@gmail.com>
2023-11-15 20:38:05 +0900

    Add: favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div>Hoge</div>
+      <h1 className="text-4xl font-bold">SyncScripter</h1>
+      <p className="text-2xl">スクリプトを同期するアプリ</p>
     </main>
   );
 }
